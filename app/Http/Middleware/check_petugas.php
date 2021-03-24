358c06de58b6dfcc97a06cf6cdf364fe651fd520
BosToken <faizdiandra11@gmail.com>
2021-03-24 09:03:17 +0700

    finale

--- app/Http/Middleware/check_petugas.php
+++ app/Http/Middleware/check_petugas.php
@@ -23,6 +23,9 @@ class check_petugas
                 return redirect('/siswa/dashboard');
             }
         }
-        return $next($request);
+        $response = $next($request);
+        return $response->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
+            ->header('Pragma','no-cache')
+            ->header('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
     }
 }
